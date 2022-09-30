2e79e7f467b60080b8e4a1ee0ec79d74577f0cbd
shondriane <shondriane.wise@yahoo.com>
2022-09-30 19:28:28 -0400

    building the deck out

--- script.js
+++ script.js
@@ -2,7 +2,28 @@
 const wallet= document.querySelector('h3')
 
 
+//Player's current value of money
  let money = 100
-// console.log(money)
  wallet.innerText = `Your current chip amount is: $${money}`;
 console.log(wallet.innerText)
+
+//build standard deck of cards
+let suites = ['hearts', 'clubs', 'spades', 'diamonds']
+let cards =['ace', '1','2', '3','4','5','6','7','8','9']
+
+
+class Card{
+    constructor (suite,card){
+        this.suite=suite
+        this.card = card
+    }
+}
+
+//iteration
+
+suites.forEach((element)=>{
+    cards.forEach((elements)=>{
+        console.log(element)
+    })
+})
+
