c464a2200a145734e1edf97226b7075515bb7978
shondriane <shondriane.wise@yahoo.com>
2022-10-01 13:25:00 -0400

    shuffle cards

--- script.js
+++ script.js
@@ -9,28 +9,37 @@ console.log(wallet.innerText)
 
 //build standard deck of cards
 let suites = ['hearts', 'clubs', 'spades', 'diamonds']
-let cards =['ace', '1','2', '3','4','5','6','7','8','9','jack', 'queen','king']
-
-
+let cards =['ace','2', '3','4','5','6','7','8','9','10','jack','queen','king']
+let cardDeck=[]
+let newDeck=[]
 
 //iteration to get full deck 
 
-const fullDeck=() =>{
-    let cardDeck=[]
-    cards.forEach((card)=>{
+ const fullDeck=() =>{
+   cards.forEach((card)=>{
         suites.forEach((suite)=>{
-           cardDeck.push(card+ ' '+ suite)
-          
-        })
-        return cardDeck;
+           cardDeck.push(card+ ' '+ suite)     
+        })   
     })
-}
+    return(cardDeck)
+  }
+ fullDeck()
 
-//shuffle cards
-let randomCard =()=>{
-  
-}
-console.log(randomCard)
+//iteration to shuffle cards and push into a new Deck
+const shuffleCards = ()=>{
+   cardDeck.forEach((cardDecks,index)=>{
+    let randomIndex=Math.floor(Math.random()*52)
+    index = randomIndex
+    let newCard;
+  newCard=cardDeck.splice(randomIndex,1,cardDecks)
+  newDeck.push(newCard)
+     })
+return newDeck
+}   
+    shuffleCards() 
+     
+
+
 
 
 
